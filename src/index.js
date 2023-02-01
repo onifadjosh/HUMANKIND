c63e4a45cd84374c64a3aac42b3f79ebdc8dfad0
onifadjosh <onifadjosh@gmail.com>
2023-02-02 00:37:48 +0100

    design changes

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ window.addEventListener('scroll', ()=>{
     window.scrollY;
     Img1.style.t
 })
+
+$(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('header').addClass('fixed-header');
+        }
+        else{
+            $('header').removeClass('fixed-header');
+        }
+    });
+});
