471a758552f0536d0d763c89f5fd2c3028060a42
onifadjosh <onifadjosh@gmail.com>
2023-02-03 16:57:51 +0100

    on scroll scaling changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const growTl = gsap.timeline({
   });
   growTl.to("#grow", {
     duration: 1,
-    scale: 1
+    scale: 1.2
   });
   growTl.to("#grow-tagline", {
     duration: 0.4,
@@ -83,14 +83,72 @@ const growTl = gsap.timeline({
 
   const growT4 = gsap.timeline({
     scrollTrigger: {
-      trigger: "#grow3",
+      trigger: "#grow4",
       scrub: 1.5,
       start: "top center",
       end: "+=400",
       ease: "power1.out"
     }
   });
-  growT4.to("#grow3", {
+  growT4.to("#grow4", {
+    duration: 1,
+    scale: 1
+  });
+
+  const growT5 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#grow5",
+      scrub: 1.5,
+      start: "top center",
+      end: "+=400",
+      ease: "power1.out"
+    }
+  });
+  growT5.to("#grow5", {
+    duration: 1,
+    scale: 1.2
+  });
+
+  const shrinkT2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#shrink2",
+      scrub: 1.5,
+      start: "top center",
+      end: "+=400",
+      ease: "power1.in"
+    }
+  });
+  
+  shrinkT2.to("#shrink2", {
+    duration: 2,
+    scale: 0.8,
+    filter: "blur(0px)"
+  });
+
+  const growT6 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#grow6",
+      scrub: 1.5,
+      start: "top center",
+      end: "+=400",
+      ease: "power1.out"
+    }
+  });
+  growT6.to("#grow6", {
+    duration: 1,
+    scale: 1.2
+  });
+
+  const growT7 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#grow7",
+      scrub: 1.5,
+      start: "top center",
+      end: "+=400",
+      ease: "power1.out"
+    }
+  });
+  growT6.to("#grow7", {
     duration: 1,
     scale: 1
   });
